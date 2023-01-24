b07fd293bf8288c5628d1e52b3b108f4cded5d63
vincent-tarrit <vincent.tarrit29@gmail.com>
2023-01-24 15:02:15 +0100

    test

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import { hexToHsl } from "./utils.js";
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   const hex = event.target.querySelector("input").value;
-  const hsl = hexToHsl(hex);
-  console.log(hsl);
+  if (/^#[0-9A-F]{6}$/i.test(hex)) {
+    const hsl = hexToHsl(hex);
+    console.log(hsl);
+  } else {
+    console.log("Invalid hex code");
+  }
 });
